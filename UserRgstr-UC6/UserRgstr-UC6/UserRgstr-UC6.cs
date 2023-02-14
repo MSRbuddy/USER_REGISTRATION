87e7b47f47cf7a6ae635e4cbc1afdf64d6024601
MSRbuddy <meghanadasari222@gmail.com>
2023-02-14 22:35:01 +0530

    UseCase-6 is added!

---
+++ UserRgstr-UC6/UserRgstr-UC6/UserRgstr-UC6.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UserRgstr_UC6
+{
+    internal class Program
+    {
+        public static string PwdRule2 = @"^[A-Z]{1,7}[a-z]{7,20}$";
+        static void Main(string[] args)
+        {
+            Regex r = new Regex(Program.PwdRule2);
+            Console.WriteLine("Enter User Password: ");
+            Console.WriteLine(r.IsMatch(Console.ReadLine()));
+        }
+    }
+}
