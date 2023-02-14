e4fb6cf85f03eb34e9dd1a4d75a390026307b4c3
MSRbuddy <meghanadasari222@gmail.com>
2023-02-14 22:02:01 +0530

    Welcome to User Registration problem!

---
+++ UserRegistration.cs
@@ -0,0 +1,10 @@
+﻿namespace UserRegistration
+{
+    internal class UserRegistration
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Welcome to User Registration problem *****");
+        }
+    }
+}
