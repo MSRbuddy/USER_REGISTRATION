5a8e1774ddecd6c94d9acc4e0f91aa682601f815
MSRbuddy <meghanadasari222@gmail.com>
2023-02-14 22:26:40 +0530

    UseCase-3 is added!

---
+++ UserRgstr-UC3/UserRgstr-UC3/UserRgstr-UC3.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UserRgstr_UC3
+{
+    internal class Program
+    {
+        public static string MailId = @"^[a-z|a-z.a-z]@[a-z|a-z.a-z]|[.a-z]$";
+        static void Main(string[] args)
+        {
+            Regex r = new Regex(Program.MailId);
+            Console.WriteLine("Enter User Email Id: ");
+            Console.WriteLine(r.IsMatch(Console.ReadLine()));
+        }
+    }
+}
