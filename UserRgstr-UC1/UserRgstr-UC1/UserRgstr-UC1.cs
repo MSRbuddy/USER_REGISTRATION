60ecfa657500aadc738235476b70f814a537acc1
MSRbuddy <meghanadasari222@gmail.com>
2023-02-14 22:19:19 +0530

    UseCase-1 is added!

---
+++ UserRgstr-UC1/UserRgstr-UC1/UserRgstr-UC1.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UserRgstr_UC1
+{
+    internal class Program
+    {
+        public static string FirstName = @"^[A-Z][A-Za-z]{3,10}$";
+        static void Main(string[] args) 
+        {
+            Regex r = new Regex(Program.FirstName);
+            Console.WriteLine("Enter User First Name: ");
+            Console.WriteLine(r.IsMatch(Console.ReadLine()));
+        }
+    }
+}
