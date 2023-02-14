ce06d463f8180540930d65ce466dd2726716aecf
MSRbuddy <meghanadasari222@gmail.com>
2023-02-14 22:32:55 +0530

    UseCase-5 is added!

---
+++ UserRgstr-UC5/UserRgstr-UC5/UserRgstr-UC5.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UserRgstr_UC5
+{
+    internal class Program
+    {
+        public static string PwdRule1 = @"^[A-Za-z]{8,20}$";
+        static void Main(string[] args)
+        {
+            Regex r = new Regex(Program.PwdRule1);
+            Console.WriteLine("Enter User Password: ");
+            Console.WriteLine(r.IsMatch(Console.ReadLine()));
+        }
+    }
+}
