3db0810f1c55db7e01b32af60fc920aa18090963
MSRbuddy <meghanadasari222@gmail.com>
2023-02-14 22:36:43 +0530

    UseCase-7 is added!

---
+++ UserRgstr-UC7/UserRgstr-UC7/UserRgstr-UC7.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UserRgstr_UC7
+{
+    internal class Program
+    {
+        public static string PwdRule3 = @"^[A-Z]{1,7}[a-z]{6,20}[0-9]{1,7}$";
+        static void Main(string[] args)
+        {
+            Regex r = new Regex(Program.PwdRule3);
+            Console.WriteLine("Enter User Password: ");
+            Console.WriteLine(r.IsMatch(Console.ReadLine()));
+        }
+    }
+}
