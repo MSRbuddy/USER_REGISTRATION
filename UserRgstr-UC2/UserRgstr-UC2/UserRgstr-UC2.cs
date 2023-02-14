1454f34f59c2f6a9e699ee89974e106078f4de55
MSRbuddy <meghanadasari222@gmail.com>
2023-02-14 22:23:28 +0530

    UseCase-2 is added!

---
+++ UserRgstr-UC2/UserRgstr-UC2/UserRgstr-UC2.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UserRgstr_UC2
+{
+    internal class Program
+    {
+        public static string LastName = @"^[A-Z][A-Za-z]{3,10}$";
+        static void Main(string[] args)
+        {
+            Regex r = new Regex(Program.LastName);
+            Console.WriteLine("Enter User Last Name: ");
+            Console.WriteLine(r.IsMatch(Console.ReadLine()));
+        }
+    }
+}
