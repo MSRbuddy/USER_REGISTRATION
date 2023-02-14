f2f341958cae47133b13cf4b12a459a673e73a8f
MSRbuddy <meghanadasari222@gmail.com>
2023-02-14 22:29:35 +0530

    UseCase-4 is added!

---
+++ UserRgstr-UC4/UserRgstr-UC4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UserRgstr_UC4
+{
+    internal class Program
+    {
+        public static string PhoneNumber = @"^[9]?[1]?[ ][0-9]{1,10}$";
+        static void Main(string[] args)
+        {
+            Regex r = new Regex(Program.PhoneNumber);
+            Console.WriteLine("Enter User Contact Number: ");
+            Console.WriteLine(r.IsMatch(Console.ReadLine()));
+        }
+    }
+}
