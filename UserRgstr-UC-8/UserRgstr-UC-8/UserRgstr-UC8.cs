95e661f099422d3ee39fbfb545286aa5bbc4ff77
MSRbuddy <meghanadasari222@gmail.com>
2023-02-14 22:38:32 +0530

    UseCase-8 is added!

---
+++ UserRgstr-UC-8/UserRgstr-UC-8/UserRgstr-UC8.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UserRgstr_UC_8
+{
+    internal class Program
+    {
+        public static string PwdRule4 = @"^[A-Z]{1,7}[a-z]{6,20}[0-9]{1,7}[-\#\$\.\%\&\8]?$";
+        static void Main(string[] args)
+        {
+            Regex r = new Regex(Program.PwdRule4);
+            Console.WriteLine("Enter User Password: ");
+            Console.WriteLine(r.IsMatch(Console.ReadLine()));
+        }
+    }
+}
