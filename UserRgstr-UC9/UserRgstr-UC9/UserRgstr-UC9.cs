329a72af5d7f37483d5a3874d73823f16aec70fc
MSRbuddy <meghanadasari222@gmail.com>
2023-02-15 21:38:43 +0530

    UseCase-9 is added!

---
+++ UserRgstr-UC9/UserRgstr-UC9/UserRgstr-UC9.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UserRgstr_UC9
+{
+    internal class UserRgstr
+    {
+        public static string SampleEmail = @"^\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z$";
+        static void Main(string[] args)
+        {
+            Regex r = new Regex(UserRgstr.SampleEmail);
+            Console.WriteLine("Enter User Sample Email: ");
+            Console.WriteLine(r.IsMatch(Console.ReadLine()));
+        }
+    }
+}
